0d9e1198097c5a7790f4a7d1d0b51fafde58af90
Yaroslav0709 <109355091+Yaroslav0709n@users.noreply.github.com>
2023-03-15 20:36:17 +0200

    Add files via upload

---
+++ Models/BurgerOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace BurgerOrderApp.Models
+{
+    public class BurgerOrder
+    {
+        public int Id { get; set; }
+        public string BurgerName { get; set; }
+        public double BurgerPrice { get; set; }
+    }
+}
---
+++ Models/BurgersModel.cs
@@ -0,0 +1,23 @@
+﻿namespace BurgerOrderApp.Models
+{
+    public class BurgersModel
+    {
+        public string BurgerImage { get; set; }
+        public string ForWhomBurger { get; set; }
+        public double BurgerPrice { get; set; }
+        public bool BeefMince { get; set; }
+        public bool LeanBeefMince { get; set; }
+        public bool Mushrooms { get; set; }
+        public bool Onions { get; set; }
+        public bool Pineapple { get; set; }
+        public bool Rosemary { get; set; }
+        public bool Tomatoes { get; set; }
+        public bool Cucumbers { get; set; }
+        public bool CheeseCheader { get; set; }
+        public bool CheeseFeta { get; set; }
+        public bool CheeseMozzarella { get; set; }
+        public bool BBQSauce { get; set; }
+        public bool TomatoSauce { get; set; }
+        public bool GreenPestoSauce { get; set; }
+    }
+}
